27ca759621c5bddd92f73af5347f35db6ef740ba
blueskiesatx <43521091+blueskiesatx@users.noreply.github.com>
2019-03-06 18:33:20 -0600

    updated app requirements
    Included starter files

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
+##This code below was all given
+#Imported dependencies. Also used in dependencies text file.
 import os
-
 import pandas as pd
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
-
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 
 #################################################
 # Database Setup
